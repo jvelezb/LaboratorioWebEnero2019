d9d5b673c98b0a4f64434fdb86d73f302d13200b
Juan Velez Ballesteros <juaball@amazon.com>
2019-01-20 23:17:01 -0600

    inicializacion de proyecto material design

---
+++ MaterialDesignProject/webpack.config.js
@@ -0,0 +1,38 @@
+const autoprefixer = require('autoprefixer');
+
+module.exports = [{
+    entry: './scss/app.scss',
+    output: {
+        // This is necessary for webpack to compile
+        // But we never use style-bundle.js
+        filename: 'style-bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'estilos.css',
+                        },
+                    },
+                    { loader: 'extract-loader' },
+                    { loader: 'css-loader' },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()]
+                        }
+                    },
+                    { loader: 'sass-loader' ,
+                        options: {
+                            includePaths: ['./node_modules']
+                        }
+                    },
+                ]
+            }
+        ]
+    },
+}];
